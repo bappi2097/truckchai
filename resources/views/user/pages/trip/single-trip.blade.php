3b910bd81b0d7ea49ea60a85d9894aa5674ca788
bappi2097 <bappi35-2097@diu.edu.bd>
2021-02-21 16:14:19 +0600

    cpanel code added

---
+++ resources/views/user/pages/trip/single-trip.blade.php
@@ -0,0 +1,37 @@
+@extends('user.layout.master')
+
+@section('content')
+<div class="col-md-10">
+    @foreach ($trips as $trip)
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card rounded px-5 py-3">
+                <div class="card-body">
+                    <span
+                        class="badge badge-{{tripStatus($trip->status)[1]}} text-uppercase p-2">{{tripStatus($trip->status)[0]}}</span>
+                    <h6 class="text-weight-bold mt-2">{{$trip->truckCategory->truckSizeCategory->size}} Feet
+                        {{$trip->truckCategory->truckWeightCategory->weight}} Ton
+                        {{$trip->truckCategory->truckCoveredCategory->name}}</h6>
+                    <p class="text-muted">{{date("F j, Y, g:i a", strtotime($trip->load_time))}}</p>
+                    <div>
+                        <span class="d-block">
+                            <span class="d-flex align-items-center">
+                                <i class="icon-circle-arrow-up text-primary"></i>
+                                <h5 class="m-0 ml-2">{{$trip->load_location}}</h5>
+                            </span>
+                        </span>
+                        <span class="seperate-icon"></span>
+                        <span class="d-block">
+                            <span class="d-flex align-items-center">
+                                <i class="icon-circle-arrow-down text-success"></i>
+                                <h5 class="m-0 ml-2">{{$trip->unload_location}}</h5>
+                            </span>
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    @endforeach
+</div>
+@endsection
